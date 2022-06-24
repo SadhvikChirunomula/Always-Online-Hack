97dd548a99187efd62490f2ca3e2d78bea782722
Sadhvik Chirunomula <53739942+SadhvikChirunomula@users.noreply.github.com>
2022-06-24 13:23:48 +0530

    adding python file

---
+++ always_online.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+import random
+
+i=0
+while True:
+	print("Python is controlling cursor from "+ repr(i) +"secs...")
+	pyautogui.moveTo(random.randint(200,800),random.randint(200,800))
+	time.sleep(30)
+	i=i+30;
