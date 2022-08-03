6c25385eec77521d6aa5463626e6101e2e698387
Sadhvik Chirunomula <53739942+SadhvikChirunomula@users.noreply.github.com>
2022-08-03 15:48:18 +0530

    adding mouse.click option

--- always_online.py
+++ always_online.py
@@ -6,5 +6,6 @@ i=0
 while True:
 	print("Python is controlling cursor from "+ repr(i) +"secs...")
 	pyautogui.moveTo(random.randint(200,800),random.randint(200,800))
+	pyautogui.click()
 	time.sleep(30)
 	i=i+30;
